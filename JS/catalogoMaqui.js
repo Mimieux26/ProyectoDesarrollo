cef4185787fe79eefadb34d53a60ae0c366eb26b
Mimieux26 <roberthsegura28@gmail.com>
2022-04-27 16:11:42 -0600

    27/04/2022 r

--- JS/catalogoMaqui.js
+++ JS/catalogoMaqui.js
@@ -1,158 +1,20 @@
 $(document).ready(function(){
 
-    $("#1").click(function(){
 
-        $.ajax({
-            url: "../PHP/consCatalogo.php",
-            method: "POST",
-            dataType:'JSON',
-            success: function(dataResponse,responseText,response,statusText){
+    $.ajax({
+        url: "../PHP/consCatalogo.php",
+        method: "POST",
+        success: function(dataResponse,responseText,response,statusText){
 
-                var maquinas = dataResponse;
+            $('#containerMaqui').append(dataResponse);
 
-                $("#VentanaTitulo").html(maquinas[0]['Nombre']);
-                $("#DesMaqu").html(maquinas[0]['Descripcion']);
-                $("#precioMaqui").html("Precio: ₡"+maquinas[0]['Precio']);
-                $("#CantidadMaquina").val("");
-            }
-        });
 
+        }
     });
 
 
-    $("#3").click(function(){
 
-        $.ajax({
-            url: "../PHP/consCatalogo.php",
-            method: "POST",
-            dataType:'JSON',
-            success: function(dataResponse,responseText,response,statusText){
-
-                var maquinas = dataResponse;
-
-                $("#VentanaTitulo").html(maquinas[1]['Nombre']);
-                $("#DesMaqu").html(maquinas[1]['Descripcion']);
-                $("#CantidadMaquina").val("");
-            }
-        });
-
-    });
-
-    $("#4").click(function(){
-
-        $.ajax({
-            url: "../PHP/consCatalogo.php",
-            method: "POST",
-            dataType:'JSON',
-            success: function(dataResponse,responseText,response,statusText){
-
-                var maquinas = dataResponse;
-
-                $("#VentanaTitulo").html(maquinas[2]['Nombre']);
-                $("#DesMaqu").html(maquinas[2]['Descripcion']);
-                $("#CantidadMaquina").val("");
-            }
-        });
-
-    });
-
-    $("#5").click(function(){
-
-        $.ajax({
-            url: "../PHP/consCatalogo.php",
-            method: "POST",
-            dataType:'JSON',
-            success: function(dataResponse,responseText,response,statusText){
-
-                var maquinas = dataResponse;
-
-                $("#VentanaTitulo").html(maquinas[3]['Nombre']);
-                $("#DesMaqu").html(maquinas[3]['Descripcion']);
-                $("#CantidadMaquina").val("");
-            }
-        });
-
-    });
-
-    $("#6").click(function(){
-
-        $.ajax({
-            url: "../PHP/consCatalogo.php",
-            method: "POST",
-            dataType:'JSON',
-            success: function(dataResponse,responseText,response,statusText){
-
-                var maquinas = dataResponse;
-
-                $("#VentanaTitulo").html(maquinas[4]['Nombre']);
-                $("#DesMaqu").html(maquinas[4]['Descripcion']);
-                $("#CantidadMaquina").val("");
-            }
-        });
-
-    });
-
-    $("#7").click(function(){
-
-        $.ajax({
-            url: "../PHP/consCatalogo.php",
-            method: "POST",
-            dataType:'JSON',
-            success: function(dataResponse,responseText,response,statusText){
-
-                var maquinas = dataResponse;
-
-                $("#VentanaTitulo").html(maquinas[5]['Nombre']);
-                $("#DesMaqu").html(maquinas[5]['Descripcion']);
-                $("#CantidadMaquina").val("");
-            }
-        });
-
-    });
-
-    $("#8").click(function(){
-
-        $.ajax({
-            url: "../PHP/consCatalogo.php",
-            method: "POST",
-            dataType:'JSON',
-            success: function(dataResponse,responseText,response,statusText){
-
-                var maquinas = dataResponse;
-
-                $("#VentanaTitulo").html(maquinas[6]['Nombre']);
-                $("#DesMaqu").html(maquinas[6]['Descripcion']);
-                $("#CantidadMaquina").val("");
-            }
-        });
-
-    });
-
-    $("#9").click(function(){
-
-        $.ajax({
-            url: "../PHP/consCatalogo.php",
-            method: "POST",
-            dataType:'JSON',
-            success: function(dataResponse,responseText,response,statusText){
-
-                var maquinas = dataResponse;
-
-                $("#VentanaTitulo").html(maquinas[7]['Nombre']);
-                $("#DesMaqu").html(maquinas[7]['Descripcion']);
-                $("#CantidadMaquina").val("");
-            }
-        });
-
-    });
-
-    $("#btn_carrito").click(function(){
-
-        // Tiene que agarrar el id del producto no se como porque es independiente
-        // Luego hacer la inserción a la base de datos
-        //Traerse la cantidad que quiere
-
-    });
+   
 
 
        
--- PHP/consCatalogo.php
+++ PHP/consCatalogo.php
@@ -2,30 +2,54 @@
 include("conexion.php");
 
 $sql="SELECT IDArti, Nombre, Descripcion, Precio, Estado FROM maquinaria";
-mysqli_query($conexion,$sql);
 
-$consulta = $conexion->prepare("Select * FROM maquinaria");
-$consulta->execute();
-$resultado = $consulta->get_result();
+$maquinas=array();
+$consulta=mysqli_query($conexion,$sql);
 
-$arregloPadre = [];
 
-    while ($maquinaria = $resultado->fetch_assoc()){
 
-        $arregloHijo = [
-            'IDArti'=> $maquinaria['IDArti'],
-            'Nombre'=>$maquinaria['Nombre'],
-            'Descripcion'=>$maquinaria['Descripcion'],
-            'Precio'=>$maquinaria['Precio'],
-            'Estado'=>$maquinaria['Estado']
+while($maquinas=mysqli_fetch_array($consulta)){
+    echo "<div class='card' style='width: 18rem;'>
+                <img class='card-img-top' src='../imagenes/tractor.jfif' alt='Card image cap'>
+                <div class='card-body' id='body-card'>
+                <h5 class='card-title'>".$maquinas["Nombre"]."</h5>
+                <button type='button' class='btn btn-primary' data-toggle='modal' data-target='#Modal".$maquinas["IDArti"]."' id='".$maquinas["IDArti"]."'> Detalles </button>
+                </div>
+            </div>
+            
+            <div class='modal fade' id='Modal".$maquinas["IDArti"]."' tabindex='-1' role='dialog' aria-labelledby='VentanaTitulo' aria-hidden='true'>
+            <div class='modal-dialog modal-dialog-centered' role='document'>
+              <div class='modal-content'>
+                <div class='modal-header'>
+                  <h5 id='VentanaTitulo'>".$maquinas["Nombre"]."</h5>
+                  <button type='button' class='close' data-dismiss='modal' aria-label='Close'>
+                    <span aria-hidden='true'>&times;</span>
+                  </button>
+                </div>
+                <div class='modal-body row'>
+                  <div class= 'col-sm-6'> 
+                    <h6 id='DesMaqu'>".$maquinas["Descripcion"]."</h6>
+                  </div>
+                  <div class='col-sm-2'>
+                    <h6 id='precioMaqui'>Precio: ₡".$maquinas["Precio"]."</h6>
+                    Cantidad:<input type='number' id='CantidadMaquina' placeholder='Ingrese cantidad'></input>
+                  </div>
+                </div>
+                <div class='modal-footer'>
+                  <button type='button' class='btn btn-secondary' data-dismiss='modal'>Cerrar</button>
+                  <button type='button'  class='fa fa-cart-plus btn btn-primary' id='btn_carrito' onclick='insert_car(".$maquinas["IDArti"].");'>Añadir al Carrito</button>
+                </div>
+              </div>
+            </div>
+          </div> ";
 
-        ];
-        array_push($arregloPadre, $arregloHijo);
+}
 
-    }
 
-    echo json_encode($arregloPadre);
+
+
+
+
 
 ?>
-
 
